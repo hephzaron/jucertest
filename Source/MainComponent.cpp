fec1184eda11cc29b6e984ab3a4784ae2581500b
Hephzaron <tobi_daramola@yahoo.com>
2021-10-26 03:36:22 +0100

    Change colour and message

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,8 +17,8 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::Font (42.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setColour (juce::Colours::red);
+    g.drawText ("Hello New PFM Student", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
